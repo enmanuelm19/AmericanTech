eb8749dbb24d53e28f261d13fc70ab2b892adef3
Gipsy Martinez <gipsy.martinez@gmail.com>
2015-06-22 00:25:05 -0430

    Galeria de instalaciones, calendario centrado, cambios en el style en javascript.. etc

--- src/main/webapp/assets/portal/js/efecto.js
+++ src/main/webapp/assets/portal/js/efecto.js
@@ -23,3 +23,27 @@
 							});
 							
 							
+							
+							var arr= ["../assets/img/caney1.jpg",
+							          "../assets/img/caney2.jpg",
+							          "../assets/img/criollas.jpg",
+							          "../assets/img/futbol.jpg",
+							          "../assets/img/piscina.jpg",
+							          "../assets/img/salon.jpg",
+							          "../assets/img/softball.jpg",
+							          "../assets/img/tennis.jpg"
+							       ];
+
+
+							          i = 0;
+							          setInterval(function(){
+							              console.log("asd")
+							              $("#imagen").attr("src", arr[i]);
+							              i++;
+							              if (i > arr.length)
+							              i = 0;
+							          },1800);		
+							
+							
+							
+							
