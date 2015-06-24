5320742ac0074e9bff763f970ca6a2c78917783c
Beatriz <beatrizencinoza@gmail.com>
2015-06-24 18:48:43 -0430

    controlador ejecutar evento, tipo sugerencia y acciones

---
+++ src/main/java/controllers/ControllerAcciones.java
@@ -0,0 +1,20 @@
+package controllers;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zul.Window;
+
+public class ControllerAcciones extends SelectorComposer<Component> {
+	private static final long serialVersionUID = 1L;
+	 
+	 @Listen("onClick = #btnRegAcciones")
+	    public void showModal(Event e) {
+	        //create a window programmatically and use it as a modal dialog.
+	        Window window = (Window)Executions.createComponents(
+	                "registrarAccion.zul", null, null);
+	        window.doModal();
+	    }
+
+}
---
+++ src/main/java/controllers/ControllerEjecutarEvento.java
@@ -0,0 +1,27 @@
+package controllers;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zul.Window;
+
+public class ControllerEjecutarEvento extends SelectorComposer<Component> {
+	private static final long serialVersionUID = 1L;
+	 
+	 @Listen("onClick = #btnMostrar1")
+	    public void showModal(Event e) {
+	        //create a window programmatically and use it as a modal dialog.
+	        Window window = (Window)Executions.createComponents(
+	                "listaTareas.zul", null, null);
+	        window.doModal();
+	    }
+	 
+	 @Listen("onClick = #btnMostrar2")
+	    public void showModal1(Event e) {
+	        //create a window programmatically and use it as a modal dialog.
+	        Window window = (Window)Executions.createComponents(
+	                "listaTareas.zul", null, null);
+	        window.doModal();
+	    }
+}
