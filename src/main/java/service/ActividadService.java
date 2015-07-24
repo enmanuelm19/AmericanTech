da70cf8d536232a4e5a8cd27fed2cec7e3bab590
Enmanuel <enmanuelm19@gmail.com>
2015-07-24 19:29:20 -0430

    Agregando algunos servicios

--- src/main/java/service/ActividadService.java
+++ src/main/java/service/ActividadService.java
@@ -1,18 +1,21 @@
+//Created by: Enmanuel Medina, Team: O'Sullivan
 package service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import models.Actividad;
+import models.TipoActividad;
 
 public class ActividadService {
 
 	static int id=0;
+	static TipoActividad tipoActividad = new TipoActividad(1,"Deporte");
 	static List<Actividad> actividades = new ArrayList<Actividad>();
 	static{
-		actividades.add(new Actividad(id++,"Caimanera de Futbol","En Proceso","Verdadero", null));
-		actividades.add(new Actividad(id++,"Caimanera de Softbol","Pendiente","Verdadero", null));
-		actividades.add(new Actividad(id++,"Caimanera de Basquet","En Proceso","Verdadero", null));
+		actividades.add(new Actividad(id++,"Caimanera de Futbol","En Proceso","Verdadero", tipoActividad));
+		actividades.add(new Actividad(id++,"Caimanera de Softbol","Pendiente","Verdadero", tipoActividad));
+		actividades.add(new Actividad(id++,"Caimanera de Basquet","En Proceso","Verdadero", tipoActividad));
 	}
 	public static List<Actividad> getActividades() {
 		return actividades;
