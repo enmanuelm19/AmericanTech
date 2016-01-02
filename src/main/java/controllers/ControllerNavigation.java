2b34b9848180e411b8208d522c764c04966d451c
Enmanuel Medina <enmanuelm19@gmail.com>
2016-01-02 16:57:26 -0430

    Correccion de la sobreposicion de las pantallas en la navegacion tipo SPA.
    Mejoras de apariencia y si estaba ladillado

--- src/main/java/controllers/ControllerNavigation.java
+++ src/main/java/controllers/ControllerNavigation.java
@@ -16,6 +16,7 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 	
 	
 	private Div contenedor1;
+	private Div contenedor;
 	private Div contenido;
 	private String pagina;
 	
@@ -25,7 +26,8 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "configuracion/perfilClub.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 
 	
@@ -33,42 +35,47 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "configuracion/juntaDirectiva.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnPreferencias(){
 		
 		pagina = "configuracion/preferencia.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnInstalaciones(){
 		
 		pagina = "configuracion/gestionarInstalaciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnProveedores(){
-		System.out.println("ola");
 		pagina = "configuracion/proveedores.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnPatrocinantes(){
 		
 		pagina = "configuracion/patrocinantes.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnReglas(){
 		
 		pagina = "configuracion/reglasClub.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	//Configuracion -> Categoria
@@ -76,91 +83,104 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "configuracion/categoria/tipoSugerencia.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 
 	public void onClick$btnTipoNoticia(){
 		
 		pagina = "configuracion/categoria/tipoNoticia.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnTipoEvento(){
 		
 		pagina = "configuracion/categoria/tipoEventoPrincipal.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnTipoPreferencia(){
 		
 		pagina = "configuracion/categoria/tipoPreferencia.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnTipoInstalacion(){
 		
 		pagina = "configuracion/categoria/tipoInstalacion.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnTipoSancion(){
 		
 		pagina = "configuracion/categoria/tiposSanciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnRecurso(){
 		
 		pagina = "configuracion/categoria/recursos.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnEstadoReservacion(){
 		
 		pagina = "configuracion/categoria/estadoReservacion.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnMotivoPostulacion(){
 		
 		pagina = "configuracion/categoria/motivoPostulacion.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnTipoActividad(){
 		
 		pagina = "configuracion/categoria/tipoActividad.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnMotivoVenta(){
 		
 		pagina = "configuracion/categoria/motivoVenta.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnTipoDesvinculacion(){
 		
 		pagina = "configuracion/categoria/tipoDesvinculacion.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnCargoJunta(){
 		
 		pagina = "configuracion/categoria/cargoJunta.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	//Socio
@@ -168,28 +188,32 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "socio/acciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnMiPerfil(){
 		
 		pagina = "socio/miPerfil.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnMisSanciones(){
 		
 		pagina = "socio/misSanciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnSolicitarVenta(){
 		
 		pagina = "socio/solicitarVenta.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	//Socio -> Administrar Socio
@@ -197,7 +221,8 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "socio/administrarSocio/postulantes.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnRegistro(){
@@ -211,56 +236,64 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "socio/administrarSocio/sanciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnOpiniones(){
 		
 		pagina = "socio/administrarSocio/mostrarOpinion.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	//Socio -> Buzon
 	public void onClick$btnSugerencias(){
 		
 		pagina = "socio/buzon/vistaGeneralSugerencias.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	//Instalacion
 	public void onClick$btnVisitas(){
 		
 		pagina = "instalacion/registrarVisita.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnMisReservaciones(){
 		
 		pagina = "instalacion/misReservaciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnAlquileres(){
 		
 		pagina = "instalacion/administrarAlquileres.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnMisAlquileres(){
 		
 		pagina = "instalacion/misAlquileres.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnEventualidades(){
 		
 		pagina = "instalacion/registrarEventualidadInst.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	//Evento
@@ -268,42 +301,48 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "evento/solicitarEvento.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnRegistrarEvento(){
 		
 		pagina = "evento/administrarEvento/registrarEvento.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnPlanificarEvento(){
 		
 		pagina = "evento/administrarEvento/planificarEvento.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnEjecutarEvento(){
 		
 		pagina = "evento/administrarEvento/ejecutarEvento.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnCancelarEvento(){
 		
 		pagina = "evento/administrarEvento/cancelarEvento.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnEventualidadEvento(){
 		
 		pagina = "instalacion/registrarEventualidadInst.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	//Administracion
@@ -311,48 +350,55 @@ public class ControllerNavigation extends GenericForwardComposer<Component>{
 		
 		pagina = "administracion/seguridad/usuarioGeneral.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnUsuarioConsulta2(){
 		
 		pagina = "administracion/seguridad/usuarioGeneral.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnUsuarioConsulta3(){
 		
 		pagina = "administracion/seguridad/usuarioGeneral.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnGrupos(){
 		
 		pagina = "administracion/seguridad/mostrarGrupos.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnFunciones(){
 		
 		pagina = "administracion/seguridad/administrarFunciones.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnEmpleados(){
 		
 		pagina = "administracion/adjuntarEmpleados.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 	
 	public void onClick$btnCartelera(){
 		
 		pagina = "administracion/cartelera.zul";
 		contenido.getChildren().clear();
-		contenido = (Div) Executions.createComponents(pagina, null, null);
+		contenedor = (Div) Executions.createComponents(pagina, null, null);
+		contenido.appendChild(contenedor);
 	}
 }
