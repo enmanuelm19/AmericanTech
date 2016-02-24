9c73d604e13607856204fed5c2a2cf5c3f67580e
Hector Gil <hectorgil188@gmail.com>
2016-02-24 08:41:18 -0430

    corrigiendo errores controlador parentesco y subiendo cambios de prisco que siempre es un peo

--- src/main/java/controllers/ControllerTipoParentesco.java
+++ src/main/java/controllers/ControllerTipoParentesco.java
@@ -1,13 +1,21 @@
 package controllers;
 
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zul.Window;
+import org.zkoss.zk.ui.util.Composer;
 
-public class ControllerTipoParentesco {
+public class ControllerTipoParentesco extends SelectorComposer<Component> {
 	
-	 @Listen("onClick = #btnRegParentesco")
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Listen("onClick = #btnRegParentesco")
 	    public void showModal(Event e) {
 	        //create a window programmatically and use it as a modal dialog.
 	        Window window = (Window)Executions.createComponents(
