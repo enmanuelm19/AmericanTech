0710c00f2918a938567ec6b54fc2988174e22e8d
MoisesMontero <moises641@gmail.com>
2016-02-24 00:11:02 -0430

    Cambios en el registro de grupo usuario y funciones

---
+++ src/main/java/controllers/FuncionViewModel.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zkoss.zul.ListModelList;
+
+import models.Funcion;
+import service.FuncionService;
+
+public class FuncionViewModel {
+	
+	private List<Funcion> funciones = new ArrayList<Funcion>();
+	
+	public ListModelList<Funcion> getFunciones(){
+		funciones = FuncionService.getFunciones();
+		return new ListModelList<Funcion>(funciones);
+		
+	}
+
+}
--- src/main/java/controllers/UsuarioViewModel.java
+++ src/main/java/controllers/UsuarioViewModel.java
@@ -1,16 +1,16 @@
 package controllers;
 
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zul.Window;
 
-public class ControllerUsuario extends SelectorComposer<Component>{
+public class UsuarioViewModel extends SelectorComposer<Component>{
 	private static final long serialVersionUID = 1L;
 
-	@Listen("onClick = #btnUsuarioGeneralReg")
+	@Command
 	public void showModal(Event e) {
 		//create a window programmatically and use it as a modal dialog.
 		Window window = (Window)Executions.createComponents(
