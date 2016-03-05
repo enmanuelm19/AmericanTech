ce56f6eaba00444660e03888ac26c7b39a04701f
Enmanuel Medina <enmanuelm19@gmail.com>
2016-03-05 16:21:02 -0430

    Modificando script de usuario_grupo para ver todas las opciones conel usuario enmanuel

--- src/BD/usuario_grupo.sql
+++ src/BD/usuario_grupo.sql
@@ -1,4 +1,8 @@
 ﻿INSERT INTO usuario_grupo(id_usuario_grupo, usuarioid_usuario, grupoid_grupo, activo)
     VALUES (1, 4, 1, true),
 	   (2 ,1 ,6 ,true),
-	   (3, 2, 4, true);
+	   (3, 2, 4, true),
+	   (4, 1, 5, true),
+	   (5, 1, 4, true),
+	   (6, 1, 2, true),
+	   (7, 1, 3, true);
