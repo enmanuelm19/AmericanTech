d6efe2375189e98ff9e352f54e0e68eabcb0a14c
Gipsy Martinez <gipsy.martinez@gmail.com>
2016-03-08 23:35:59 -0430

    Portal con texto para cada instalacion, script para bd portal

--- src/main/java/controllers/IndexPortalViewModel.java
+++ src/main/java/controllers/IndexPortalViewModel.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import modelos.Club;
+
 
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.BindingParam;
@@ -20,32 +20,49 @@ import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Window;
 
+import modelos.Club;
+import modelos.Portal;
 import Dao.ClubDao;
+import Dao.PortalDao;
 
 
 public class IndexPortalViewModel {
 
-//	private List<TipoPreferencia> tiposAll;
-//	private TipoPreferenciaDao tipoDao;
-//	private String descFiltro;
-//	private String idFiltro;
 	private List<Club> clubAll;
+	//private List<Portal> portalAll;
 	private ClubDao clubDao;
-//	private 
+	private Portal portal;
+	private Club club;
+	private PortalDao portalDao;
 
 	@Init
 	public void init() throws Exception {
 		
 		clubAll = new ArrayList<Club>();
+		club = new Club();
+		portal = new Portal();
 		clubDao = new ClubDao();
+		portalDao = new PortalDao();
 		clubAll = clubDao.obtenerTodos();
+		int id = 1;
+		portal = portalDao.obtenerPortal(id);
+		int idClub = 1;
+		club = clubDao.obtenerClub(idClub);
+		
 	}
 
 	public ListModelList<Club> getAllClub() {
 		return new ListModelList<Club>(clubAll);
 	}
 	
-
+	public Portal getPortal() {
+		return portal;
+	}
+	
+	public Club getClub(){
+		return club;
+	}
+	
 	
 	
 }	
