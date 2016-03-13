e4f201824831a9a3e31a7d6e3f1ede465f0cdea2
roman <romanbarriosj@gmail.com>
2016-03-13 16:17:29 -0430

    reporte estructurado eventualidades

---
+++ src/main/java/controllers/ReporteEventualidadesViewModel.java
@@ -0,0 +1,119 @@
+package controllers;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.zkoss.bind.BindUtils;
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Messagebox;
+import org.zkoss.zul.Window;
+
+
+
+
+
+
+
+
+
+import Dao.InstalacionDao;
+import modelos.Evento;
+import modelos.Instalacion;
+import modelos.Socio;
+import modelos.TipoInstalacion;
+import modelos.TipoPreferencia;
+
+
+
+public class ReporteEventualidadesViewModel {
+	
+
+	private Socio socio;
+	private int tipo;
+	private Time horaInicio;
+	private Time horaFin;
+	private Date fechaInicio;
+	private Date fechaFin;
+	private InstalacionDao instalacionDao;
+	private TipoInstalacion tipoInstalacionSelected;
+
+
+	@Init
+	public void init() {
+		instalacionDao = new InstalacionDao();
+	}
+
+	public ListModelList<Instalacion> getInstalaciones() throws Exception {
+
+		return new ListModelList<Instalacion>(instalacionDao.obtenerTodos());
+
+	}
+	
+	@NotifyChange("tipoInstalacionSelected")
+	public TipoInstalacion getTipotipoInstalacionSelected() {
+		return tipoInstalacionSelected;
+	}
+
+	@NotifyChange("InstalacionPorTipo")
+	public void setTipoInstalacionSelected(TipoInstalacion tipoInstalacionSelected) {
+		this.tipoInstalacionSelected = tipoInstalacionSelected;
+	}
+
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public Time getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(Time horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public Time getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(Time horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public Socio getSocio() {
+		return socio;
+	}
+
+	public void setSocio(Socio socio) {
+		this.socio = socio;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+}
