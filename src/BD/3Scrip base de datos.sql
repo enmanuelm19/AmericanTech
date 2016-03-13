69cf7bab254bb926a50b590a306ee77045abddea
Edward Rodriguez <edwardarg90@gmail.com>
2016-03-13 10:05:55 -0430

    POR LO QUE MAS QUIERAN NO MODIFIQUEN LOS SCRIPTS,  si le colocan claves primarias el proyecto probablemente explote durante su ejecucion

--- src/BD/3Scrip base de datos.sql
+++ src/BD/3Scrip base de datos.sql
@@ -83,6 +83,7 @@ insert into instalacion (nombre,descripcion,capacidad,precio_alquiler,tipo_insta
 
 insert into tipo_preferencia (descripcion,color,activo) values ('Cultural','#008000',true);
 insert into tipo_preferencia (descripcion,color,activo) values ('Deportiva','#0000FF',true);
+insert into tipo_preferencia (descripcion,color,activo) values ('Social','#20B2AA',true);
 
 --Preferencia
 
@@ -126,12 +127,12 @@ insert into venta (accionid_accion,postulacionid_postulacion,fecha,monto,activo)
 
 --tipo_afiliado
 
-insert into tipo_afiliado (descripcion,activo) values ('Pareja',true);
-insert into tipo_afiliado (descripcion,activo) values ('Hijo',true);
+insert into tipo_afiliado (descripcion,subfijo,activo) values ('Pareja','01',true);
+insert into tipo_afiliado (descripcion,subfijo,activo) values ('Hijo','02',true);
 
 --afiliado
 insert into afiliado (tipo_afiliadoid_tipo_afiliado,socioid_socio,nro_carnet,personaid_persona,activo) values (1,1,'B-033801',8,true);
-insert into afiliado (tipo_afiliadoid_tipo_afiliado,socioid_socio,nro_carnet,personaid_persona,activo) values (2,3,'B-0340',9,true);
+insert into afiliado (tipo_afiliadoid_tipo_afiliado,socioid_socio,nro_carnet,personaid_persona,activo) values (2,4,'B-0340',9,true);
 
 --empleado
 insert into empleado (personaid_persona,tipo_empleadoid_tipo_empleado,fecha_contratado,activo) values (10,1,'02/03/2016',true);
@@ -145,12 +146,23 @@ insert into cargo (descripcion,activo) values ('tesorero',true);
 
 --tipo_eventualidad
 
-insert into tipo_eventualidad (descripcion,activo) values ('Deuda',true);
-insert into tipo_eventualidad (descripcion,activo) values ('Perdida de Recursos',true);
-insert into tipo_eventualidad (descripcion,activo) values ('Mala Conducta',true);
+insert into tipo_eventualidad (descripcion,activo) 
+                       values ('Deuda',true),
+ 							  ('Perdida de Recursos',true),
+							  ('Mala Conducta',true),
+							  ('Actos Inmorales', TRUE),
+           					  ('Daños a Instalacion', TRUE);
 
 --eventualidad
-insert into eventualidad (instalacionid_instalacion,descripcion,fecha,personaid_persona,tipo_eventualidadid_tipo_eventualidad,activo) values (1,'Perdida de 3 sillas','01/02/2016',3,2,true);
+insert into eventualidad (instalacionid_instalacion,descripcion,fecha,personaid_persona,tipo_eventualidadid_tipo_eventualidad,activo) 
+				  values (1,'Perdida de 3 sillas','01/02/2016',3,2,true),
+				  		 (2, 'Partió 3 sillas', '01-03-2016 09:00:00',5,5,TRUE),
+	   			  		 (1, 'Riña con otro jugador', '04-03-2016 11:00:00',12,3,TRUE),
+	   			  		 (3, 'Uso inadecuado de instalación', '05-03-2016 16:00:00',7,5,TRUE),
+	   			 		 (3, 'Caminó desnudo dentro de la instalacion', '08-03-2016 18:00:00',6,4,TRUE),
+	   			  		 (1, 'Hurto de pelota de Baseball', '11-03-2016 19:00:00',14,2,TRUE),
+	   			 		 (2, 'Riña con otro socio', '16-03-2016 21:00:00',13,3,TRUE),
+	   			 		 (1, 'Hizo un hueco en el campo', '17-03-2016 22:00:00',15,5,TRUE);
 
 --sancion
 insert into sancion (descripcion,socioid_socio,fecha_inic,fecha_fin,monto,eventualidadid_eventualidad,tipo_sancionid_tipo_sancion,activo) values ('Deuda del mes de Enero y Febrero 2016',3,'16/02/2016','08/02/2016',3000,1,1,true);
@@ -193,8 +205,8 @@ insert into tipo_noticia (descripcion,color,activo) values ('Otros','#FFA500',tr
 
 
 --Evento
-insert into evento (nombre,descripcion,fecha_inicio,fecha_fin,publico,estado_eventoid_estado_evento,cancelacion_eventoid_cancelacion_evento,activo) values ('Torneo de Beisbol','Torneo Libre Masculino de Beisbol','10/03/2016','10/06/2016',true,2,null,true);
-insert into evento (nombre,descripcion,fecha_inicio,fecha_fin,publico,estado_eventoid_estado_evento,cancelacion_eventoid_cancelacion_evento,activo) values ('Torneo de Tenis','Torneo Libre Femenino de tenis','10/03/2016','10/06/2016',true,2,null,true);
+insert into evento (nombre,descripcion,fecha_inicio,fecha_fin,publico,estado_eventoid_estado_evento,cancelacion_eventoid_cancelacion_evento,activo) values ('Torneo de Beisbol','Torneo Libre Masculino de Beisbol','10/03/2016','10/06/2016',true,4,null,true);
+insert into evento (nombre,descripcion,fecha_inicio,fecha_fin,publico,estado_eventoid_estado_evento,cancelacion_eventoid_cancelacion_evento,activo) values ('Torneo de Tenis','Torneo Libre Femenino de tenis','10/03/2016','10/06/2016',true,4,null,true);
 
 --actividad
 insert into actividad (descripcion,finalizada,tipo_actividadid_tipo_actividad,eventoid_evento,fecha_tope,fecha_realizacion,valor_esperado,valor_real,activo) values ('mantenimiento al campo de Beisbol',false,1,1,'10/03/2016','08/03/2016',2000,2200,true);
@@ -202,3 +214,75 @@ insert into actividad (descripcion,finalizada,tipo_actividadid_tipo_actividad,ev
 --Instalacion_evento
 
 insert into instalacion_evento (instalacionid_instalacion,eventoid_evento,activo) values (1,1,true);
+
+--Sanciones
+INSERT INTO sancion(descripcion,socioid_socio,fecha_inic,fecha_fin,  tipo_sancionid_tipo_sancion,activo)
+    VALUES ('Se agarro a golpes con otro socio en medio del partido', 2, '01-01-2016', '10-01-2016', 3, TRUE),
+	   ('Destrozo la propiedad', 2, '02-01-2016', '12-01-2016', 3, TRUE),
+	   ('Se orino en la piscina', 2, '04-01-2016', '10-01-2016', 2, TRUE),
+	   ('Insulto a un directivo del Club', 1, '06-01-2016', '30-01-2016', 1, TRUE),
+	   ('Borracho molesto a unas muchachas', 1, '01-01-2016', '10-01-2016', 1, TRUE);
+
+--tipo_pago
+INSERT INTO tipo_pago(descripcion, activo)
+    VALUES ('Efectivo', TRUE),
+	   ('Transferencia', TRUE),
+	   ('Deposito', TRUE);
+---reservaciones
+INSERT INTO reservacion(instalacionid_instalacion, socioid_socio, fecha_inicio, fecha_fin, activo)
+    VALUES (1, 2, '01-01-2016', '01-01-2016', TRUE),
+	   (2, 3, '01-01-2016', '01-01-2016', TRUE),
+	   (3, 1, '01-01-2016', '01-01-2016', TRUE),
+	   (1, 4, '10-01-2016', '10-01-2016', TRUE),
+	   (2, 2, '10-01-2016', '10-01-2016', TRUE),
+	   (3, 1, '10-01-2016', '10-01-2016', TRUE),
+	   (1, 3, '12-01-2016', '12-01-2016', TRUE),
+	   (2, 3, '12-01-2016', '12-01-2016', TRUE),
+	   (3, 4, '12-01-2016', '12-01-2016', TRUE);
+
+--alquileres
+
+INSERT INTO alquiler(reservacionid_reservacion, tipo_pagoid_tipo_pago, fecha, monto, activo)
+    VALUES (1, 1, '05-01-2016', 2000, TRUE),
+	   (2, 2, '05-01-2016', 20000, TRUE),
+	   (3, 3, '07-01-2016', 2000, TRUE),
+	   (4, 1, '15-01-2016', 2000, TRUE),
+	   (5, 2, '16-01-2016', 20000, TRUE),
+	   (6, 3, '17-01-2016', 2000, TRUE),
+	   (7, 1, '18-01-2016', 2000, TRUE),
+	   (8, 2, '19-01-2016', 20000, TRUE),
+	   (9, 3, '20-01-2016', 2000, TRUE);
+
+--indicador
+INSERT INTO indicador(descripcion, activo)
+    VALUES ('Asistencia de Personas', TRUE),
+           ('Ingresos Recolectados', TRUE),
+           ('Utilidad', TRUE),
+           ('Ingresos por Incripcion a Torneos', TRUE),
+           ('Ingresos por Bebidas alcoholicas', TRUE);
+
+-- preferencia_evento
+INSERT INTO preferencia_evento(eventoid_evento, preferenciaid_preferencia, 
+            activo)
+    VALUES ( 1, 1, TRUE),
+           ( 2, 3, TRUE);
+
+--indicador_evento
+
+INSERT INTO indicador_evento(valor_esperado, valor_real, indicadorid_indicador, 
+            eventoid_evento, activo)
+    VALUES (30000000, 25000000, 2, 1, TRUE),
+           (300000, 200000, 4, 1, TRUE),
+           (2500, 1850, 1, 2, TRUE),
+           (300000, 350000, 3, 2, TRUE),
+           (200000, 300000, 4, 2, TRUE);
+           
+--Mas Afiliados          
+INSERT INTO afiliado(tipo_afiliadoid_tipo_afiliado, socioid_socio, nro_carnet, 
+            personaid_persona, activo)
+    VALUES (1, 2, 'B-0339-01', 5, TRUE),
+           (2, 2, 'B-0339-02', 7, TRUE),
+           (1, 3, 'B-0340-01', 11, TRUE),
+           (2, 3, 'B-0340-02', 13, TRUE);
+
+
