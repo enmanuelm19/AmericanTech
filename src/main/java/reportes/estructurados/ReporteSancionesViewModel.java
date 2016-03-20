2e7d5d7748a768fb1d04e4fb87469350f798f158
FranMoron <josefranciscomoron@gmail.com>
2016-03-20 11:12:11 -0430

    reporte sancion

--- src/main/java/reportes/estructurados/ReporteSancionesViewModel.java
+++ src/main/java/reportes/estructurados/ReporteSancionesViewModel.java
@@ -1,10 +1,33 @@
 package reportes.estructurados;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JRDesignQuery;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.BindingParam;
@@ -12,9 +35,16 @@ import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zhtml.Filedownload;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Window;
+
+
+
+
+
 
 
 
@@ -44,6 +74,17 @@ public class ReporteSancionesViewModel {
 	private boolean nrocarnet;
 	private boolean sancionados;
 	private boolean check;
+	
+	//reporte
+	private String sql = "";
+	private String consulta = "";
+	private String titulo = "Sanciones";
+	private String reporte;
+	private Connection con;
+	private Map<String, Object> parameters = new HashMap<String, Object>();
+	private File img = new File(System.getProperty("user.home") + "/reportes_america/imagen_club.png");
+	private File img2 = new File(System.getProperty("user.home") + "/reportes_america/imagen_equipo.png");
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"), sdfGuio = new SimpleDateFormat("dd-MM-yyyy");
 
 
 	@Init
@@ -144,5 +185,62 @@ public class ReporteSancionesViewModel {
 		}
 		
 	}
+	
+	@Command
+	public void btnPDF(Event e) throws SQLException, JRException, IOException {
+		cargarSql();
+	}
+	
+	public void cargarSql() throws FileNotFoundException, JRException, SQLException{
+		try {
+			Class.forName ("org.postgresql.Driver");
+		
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/America","postgres","postgres");
+			
+			
+		} catch (ClassNotFoundException el) {
+			el.printStackTrace();
+		}
+		if (this.tipo == null){
+			Messagebox.show("Seleccione una opcion: Todos, Socios ó Afiliados", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+			
+		}else if(this.tipo.equalsIgnoreCase("1")){
+			this.titulo = "SOCIOS Y AFILIADOS SANCIONADOS";
+			this.consulta= "socios y afiliados sancionados ";
+			//reporte = System.getProperty("user.home") + "/reportes_america/sanciones_socios_afiliados.jrxml";
+		}	
+		
+	}
+	
 
+	public void generarPDF() throws JRException, FileNotFoundException, SQLException {
+		System.out.println(sql);
+		Date hoy = (Date) Calendar.getInstance().getTime();
+		String date = "-"+sdfGuio.format(hoy).toString();
+		String nombreArchivo = this.titulo.concat(date);
+		JasperPrint jasperPrint = cargarJasper();
+		
+		JRExporter exporter = new JRPdfExporter();
+	    Filedownload.save(JasperExportManager.exportReportToPdf(jasperPrint), "application/pdf", nombreArchivo+".pdf"); 
+	    con.close();
+	}
+	
+	public JasperPrint cargarJasper() throws JRException, FileNotFoundException{
+		JasperDesign jd = null;  
+		jd = JRXmlLoader.load(reporte); 
+		JRDesignQuery newQuery = new JRDesignQuery();  
+		newQuery.setText(sql);  
+		jd.setQuery(newQuery); 
+		JasperReport jasperRepor = JasperCompileManager.compileReport(jd);
+		parameters.clear();
+		FileInputStream image_club = new FileInputStream(img);
+		FileInputStream imagen_equipo = new FileInputStream(img2);
+		parameters.put("TITULO", titulo);
+		parameters.put("CONSULTA", consulta);
+		parameters.put("IMAGEN_EQUIPO", imagen_equipo );
+		parameters.put("IMAGEN_CLUB", image_club);
+		return  JasperFillManager.fillReport(jasperRepor, parameters, con);
+		
+		
+	}
 }
