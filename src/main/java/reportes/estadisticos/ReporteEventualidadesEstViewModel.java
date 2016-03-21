42427885f8393d9a3a37987d311262b49523c68f
suarezcumare <suarezcumare@gmail.com>
2016-03-20 20:23:02 -0430

    actualizando reportes

--- src/main/java/reportes/estadisticos/ReporteEventualidadesEstViewModel.java
+++ src/main/java/reportes/estadisticos/ReporteEventualidadesEstViewModel.java
@@ -1,10 +1,33 @@
 package reportes.estadisticos;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JRDesignQuery;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.BindingParam;
@@ -13,9 +36,13 @@ import org.zkoss.bind.annotation.DependsOn;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zhtml.Filedownload;
+import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Window;
+
+
 
 
 
@@ -52,6 +79,19 @@ public class ReporteEventualidadesEstViewModel {
 	private boolean horaHasta;
 	private boolean instalacioncheck;
 	private boolean instalacion;
+	private int horaDesdeSelect, horaHastaSelect;
+	
+	//reporte
+		private String sql = "";
+		
+		private String consulta = "";
+		private String titulo = "Sanciones";
+		private String reporte;
+		private Connection con;
+		private Map<String, Object> parameters = new HashMap<String, Object>();
+		private File img = new File(System.getProperty("user.home") + "/reportes_america/imagen_club.png");
+		private File img2 = new File(System.getProperty("user.home") + "/reportes_america/imagen_equipo.png");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"), sdfGuio = new SimpleDateFormat("dd-MM-yyyy");
 
 
 	@Init
@@ -214,7 +254,137 @@ public class ReporteEventualidadesEstViewModel {
 	public void setInstalacion(boolean instalacion) {
 		this.instalacion = instalacion;
 	}
+	
+	@NotifyChange("horaDesdeSelect")
+	public int getHoraDesdeSelect() {
+		return horaDesdeSelect;
+	}	
+
+	@NotifyChange("horaDesdeSelect")
+	public void  seHhoraDesdeSelect(int horaDesdeSelect) {
+		this.horaDesdeSelect = horaDesdeSelect;
+	}
+	
+	@NotifyChange("horaHastaSelect")
+	public int getHoraHastaSelect() {
+		return horaHastaSelect;
+	}
+	
+	@NotifyChange("horaHastaSelect")
+	public void  setHoraHastaSelect(int edadHastaSelected) {
+		this.horaHastaSelect = horaHastaSelect;
+	}
 
 	
+	@Command
+	public void btnPDF(Event e) throws SQLException, JRException, IOException {
+		System.out.println("TONY");
+		cargarSql();
+		
+	}
 	
+	public void cargarSql() throws FileNotFoundException, JRException, SQLException{
+		try {
+			Class.forName ("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/America","postgres","postgres");
+		} catch (ClassNotFoundException el) {
+			el.printStackTrace();
+		}
+		
+		System.out.println("hora: "+ this.horaDesdeSelect);
+		System.out.println("hora: "+ this.horaDesdeSelect);
+		if(this.fechaDesde == null && this.fechaHasta == null){
+			reporte = System.getProperty("user.home") + "/reportes_america/estadistico_eventualidades1.jrxml";
+			sql = "select i.nombre as Instalacion, COUNT(e.*), (COUNT(e.*) *100) /(select COUNT(*) from eventualidad) as Porcentaje "
+					+ "from instalacion i, eventualidad e "
+					+ "where i.id_instalacion = e.instalacionid_instalacion and "
+					+ "i.activo = e.activo = TRUE  group by i.id_instalacion";
+			this.consulta += "Reporte general de eventualidaes sin un rango de fechas.";
+			generarPDF();
+			
+		} else if (this.fechaDesde == null || this.fechaHasta == null){
+			Messagebox.show("Debe Seleccionar el rango de fechas", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+		} else if (this.fechaDesde.compareTo(this.fechaHasta) > 1 ){
+			Messagebox.show("Fecha Desde no puede ser mayor a la Fecha Hasta", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+		} else {
+			this.consulta += "Reporte de eventualidaes entre las fechas " + sdf.format(this.fechaDesde) + " y "+ sdf.format(this.fechaHasta)+".";
+			
+			sql = "select i.nombre as Instalacion, COUNT(e.*), "
+					+ "(COUNT(e.*) *100) /(select COUNT(*) from eventualidad "
+					+ "where fecha between '03/01/2015' and '03/30/2016') as Porcentaje "
+					+ "from instalacion i, eventualidad e "
+					+ "where i.id_instalacion = e.instalacionid_instalacion "
+					+ "and i.activo = e.activo = TRUE "
+					+ "and e.fecha between '"+sdf.format(this.fechaDesde)+"' and '"+  sdf.format(this.fechaHasta) +"' group by i.id_instalacion ";
+			
+		
+			generarPDF();
+		}
+		
+	}
+	
+	public void sqlHORA() throws FileNotFoundException, JRException, SQLException{
+		
+		if(this.fechaDesde == null && this.fechaHasta == null){
+			reporte = System.getProperty("user.home") + "/reportes_america/estadistico_eventualidades1.jrxml";
+			sql = "select i.nombre as Instalacion, COUNT(e.*), (COUNT(e.*) *100) /(select COUNT(*) from eventualidad) as Porcentaje "
+					+ "from instalacion i, eventualidad e "
+					+ "where i.id_instalacion = e.instalacionid_instalacion and "
+					+ "i.activo = e.activo = TRUE  group by i.id_instalacion";
+			this.consulta += "Reporte general de eventualidaes sin un rango de fechas.";
+			generarPDF();
+			
+		} else if (this.fechaDesde == null || this.fechaHasta == null){
+			Messagebox.show("Debe Seleccionar el rango de fechas", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+		} else if (this.fechaDesde.compareTo(this.fechaHasta) > 1 ){
+			Messagebox.show("Fecha Desde no puede ser mayor a la Fecha Hasta", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+		} else {
+			this.consulta += "Reporte de eventualidaes entre las fechas " + sdf.format(this.fechaDesde) + " y "+ sdf.format(this.fechaHasta)+".";
+			
+			sql = "select i.nombre as Instalacion, COUNT(e.*), "
+					+ "(COUNT(e.*) *100) /(select COUNT(*) from eventualidad "
+					+ "where fecha between '03/01/2015' and '03/30/2016') as Porcentaje "
+					+ "from instalacion i, eventualidad e "
+					+ "where i.id_instalacion = e.instalacionid_instalacion "
+					+ "and i.activo = e.activo = TRUE "
+					+ "and e.fecha between '"+sdf.format(this.fechaDesde)+"' and '"+  sdf.format(this.fechaHasta) +"' group by i.id_instalacion ";
+			
+		
+			generarPDF();
+		}
+		
+//		select DISTINCT ((select COUNT(*) from eventualidad e where date_part('hour', e.fecha)< 16) * 100) / (select COUNT(*) from eventualidad e)  as Antes,
+//		  ((select COUNT(*) from eventualidad e where date_part('hour', e.fecha) between 16 and 19)  * 100) / (select COUNT(*) from eventualidad e)  as Seleccion,
+//                ((select COUNT(*) from eventualidad e where date_part('hour', e.fecha)> 19)  * 100) / (select COUNT(*) from eventualidad e)  as Despues
+//from eventualidad e
+//where e.fecha between '03-01-2015' and '12-30-2016' and e.activo = true;
+	}
+	
+	public void generarPDF() throws JRException, FileNotFoundException, SQLException {
+		Date hoy = (Date) Calendar.getInstance().getTime();
+		String date = "-"+sdfGuio.format(hoy).toString();
+		String nombreArchivo = this.titulo.concat(date);
+		JasperPrint jasperPrint = cargarJasper();
+		
+		JRExporter exporter = new JRPdfExporter();
+	    Filedownload.save(JasperExportManager.exportReportToPdf(jasperPrint), "application/pdf", nombreArchivo+".pdf"); 
+	    con.close();
+	}
+	
+	public JasperPrint cargarJasper() throws JRException, FileNotFoundException{
+		JasperDesign jd = null;  
+		jd = JRXmlLoader.load(reporte); 
+		JRDesignQuery newQuery = new JRDesignQuery();  
+		newQuery.setText(sql);  
+		jd.setQuery(newQuery); 
+		JasperReport jasperRepor = JasperCompileManager.compileReport(jd);
+		parameters.clear();
+		FileInputStream image_club = new FileInputStream(img);
+		FileInputStream imagen_equipo = new FileInputStream(img2);
+		parameters.put("TITULO", titulo);
+		parameters.put("CONSULTA", consulta);
+		parameters.put("IMAGEN_EQUIPO", imagen_equipo );
+		parameters.put("IMAGEN_CLUB", image_club);
+		return  JasperFillManager.fillReport(jasperRepor, parameters, con);
+	}
 }
--- src/main/java/reportes/estadisticos/ReporteSancionesEstViewModel.java
+++ src/main/java/reportes/estadisticos/ReporteSancionesEstViewModel.java
@@ -1,10 +1,33 @@
 package reportes.estadisticos;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JRDesignQuery;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.BindingParam;
@@ -12,6 +35,11 @@ import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zhtml.Filedownload;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zkex.zul.Jasperreport;
+import org.zkoss.zkex.zul.JasperreportExporterFactory;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Window;
@@ -22,11 +50,25 @@ import org.zkoss.zul.Window;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+import antlr.collections.List;
 import Dao.InstalacionDao;
 import Dao.PersonaDao;
+import Dao.SancionDao;
 import Dao.SocioDao;
 import modelos.Instalacion;
 import modelos.Persona;
+import modelos.Sancion;
 import modelos.Socio;
 import modelos.TipoInstalacion;
 import modelos.TipoPreferencia;
@@ -45,6 +87,17 @@ public class ReporteSancionesEstViewModel {
 	private InstalacionDao instalacionDao;
 	private TipoInstalacion tipoInstalacionSelected;
 
+	//reporte
+		private String sql = "";
+		
+		private String consulta = "";
+		private String titulo = "Sanciones";
+		private String reporte;
+		private Connection con;
+		private Map<String, Object> parameters = new HashMap<String, Object>();
+		private File img = new File(System.getProperty("user.home") + "/reportes_america/imagen_club.png");
+		private File img2 = new File(System.getProperty("user.home") + "/reportes_america/imagen_equipo.png");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"), sdfGuio = new SimpleDateFormat("dd-MM-yyyy");
 
 
 	@Init
@@ -129,9 +182,80 @@ public class ReporteSancionesEstViewModel {
 			if(this.socio==null){
 				Messagebox.show("Carnet encontrado", "Warning", Messagebox.OK, Messagebox.EXCLAMATION);
 			}
-
 		}
 	}
-
+	
+	@Command
+	public void btnPDF(Event e) throws SQLException, JRException, IOException {
+		System.out.println("TONY");
+		cargarSql();
+		
+	}
+	
+	public void cargarSql() throws FileNotFoundException, JRException, SQLException{
+		try {
+			Class.forName ("org.postgresql.Driver");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/America","postgres","postgres");
+		} catch (ClassNotFoundException el) {
+			el.printStackTrace();
+		}
+		
+		reporte = System.getProperty("user.home") + "/reportes_america/estadistico_sanciones.jrxml";
+		if(this.fechadesde == null && this.fechahasta == null){
+			
+			sql = "SELECT t.descripcion, COUNT(s.*) as count, "
+					+ "  (COUNT(s.*) * 100) / (SELECT COUNT(*) "
+					+ "FROM sancion) as porcentaje "
+					+ "FROM sancion s, tipo_sancion t "
+					+ "WHERE s.tipo_sancionid_tipo_sancion = t.id_tipo_sancion AND s.activo = t.activo = TRUE "
+					+ " GROUP BY t.id_tipo_sancion";
+			this.consulta = "Reporte general de las sanciones";
+			generarPDF();
+			
+		} else if (this.fechadesde == null || this.fechahasta == null){
+			Messagebox.show("Debe Seleccionar el rango de fechas", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+		} else if (this.fechadesde.compareTo(this.fechahasta) > 1 ){
+			Messagebox.show("Fecha Desde no puede ser mayor a la Fecha Hasta", "warning", Messagebox.OK, Messagebox.EXCLAMATION);
+		} else {
+			this.consulta += "Reporte general de las sanciones entre las fechas " + sdf.format(this.fechadesde) + " y "+ sdf.format(this.fechahasta)+".";
+			sql = "SELECT t.descripcion, COUNT(s.*) as count,"
+					+ "  (COUNT(s.*) * 100) / (SELECT COUNT(*) "
+					+ "FROM sancion WHERE fecha_inic BETWEEN '"+sdf.format(this.fechadesde)+"' AND '"+  sdf.format(this.fechahasta) +"') as porcentaje "
+					+ "FROM sancion s, tipo_sancion t "
+					+ "WHERE s.tipo_sancionid_tipo_sancion = t.id_tipo_sancion AND s.activo = t.activo = TRUE "
+					+ "AND s.fecha_inic BETWEEN '"+sdf.format(this.fechadesde)+"' AND '"+  sdf.format(this.fechahasta) +"' GROUP BY t.id_tipo_sancion";
+			System.out.println(sql);
+			generarPDF();
+		}
+		
+	}
+	
+	public void generarPDF() throws JRException, FileNotFoundException, SQLException {
+		Date hoy = (Date) Calendar.getInstance().getTime();
+		String date = "-"+sdfGuio.format(hoy).toString();
+		String nombreArchivo = this.titulo.concat(date);
+		JasperPrint jasperPrint = cargarJasper();
+		
+		JRExporter exporter = new JRPdfExporter();
+	    Filedownload.save(JasperExportManager.exportReportToPdf(jasperPrint), "application/pdf", nombreArchivo+".pdf"); 
+	    con.close();
+	}
+	
+	public JasperPrint cargarJasper() throws JRException, FileNotFoundException{
+		JasperDesign jd = null;  
+		jd = JRXmlLoader.load(reporte); 
+		JRDesignQuery newQuery = new JRDesignQuery();  
+		newQuery.setText(sql);  
+		jd.setQuery(newQuery); 
+		JasperReport jasperRepor = JasperCompileManager.compileReport(jd);
+		parameters.clear();
+		FileInputStream image_club = new FileInputStream(img);
+		FileInputStream imagen_equipo = new FileInputStream(img2);
+		parameters.put("TITULO", titulo);
+		parameters.put("CONSULTA", consulta);
+		parameters.put("IMAGEN_EQUIPO", imagen_equipo );
+		parameters.put("IMAGEN_CLUB", image_club);
+		return  JasperFillManager.fillReport(jasperRepor, parameters, con);
+	}
 
 }
