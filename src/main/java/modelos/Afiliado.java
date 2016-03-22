e7798d7500c071bcf6456cf64660b35bec45fef5
prisco-casares <prisco.casares@gmail.com>
2016-03-22 17:14:00 -0430

    listo mi perfil y agregado el modal de cambiar clave

--- src/main/java/modelos/Afiliado.java
+++ src/main/java/modelos/Afiliado.java
@@ -1,5 +1,8 @@
 package modelos;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -97,6 +100,31 @@ public class Afiliado implements java.io.Serializable {
 
 	public void setSancions(Set<Sancion> sancions) {
 		this.sancions = sancions;
+	}
+	
+	public int getCalcularEdadAfiliado() {
+		Calendar birth = new GregorianCalendar();
+		Calendar today = new GregorianCalendar();
+		int age=0;
+		int factor=0;
+		Date birthDate= this.persona.getFechaNac();
+		Date currentDate= new Date(); //current date
+		birth.setTime(birthDate);
+		today.setTime(currentDate);
+		if (today.get(Calendar.MONTH) <= birth.get(Calendar.MONTH)) {
+			if (today.get(Calendar.MONTH) == birth.get(Calendar.MONTH)) {
+				if (today.get(Calendar.DATE) > birth.get(Calendar.DATE)) {
+					factor = -1; //Aun no celebra su cumpleaños
+				}
+			} else {
+		factor = -1; //Aun no celebra su cumpleaños
+		}
+		
+		} 
+		age=(today.get(Calendar.YEAR)-birth.get(Calendar.YEAR))+factor;
+		return age;
+	
+	
 	}
 
 }
